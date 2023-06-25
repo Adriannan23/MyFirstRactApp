0ac4980af28a2551fc5b210e57eefa00883d2d41
Adrianna Nowak <adriannanowakk@gmail.com>
2023-06-25 23:42:13 +0200

    add ' case 'ADD_CARD':' to the store.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,6 +6,7 @@ import { useSelector } from 'react-redux';
 const List = () => {
 
   const columns = useSelector(state => state.columns);
+  const cards = useSelector(state => state.cards);
 
   return (
     <div className={styles.list}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,21 @@
 import { createStore } from 'redux';
 import initialState from './initialState';
+import shortid from 'shortid';
 
+
+// to tu przechowujemy całą logikę do zmiany danych z centrali
+// modyfikacja danych
 const reducer = (state, action) => {
-  return state;
+
+  switch (action.type) {
+
+    case 'ADD_COLUMN':
+      return { ...state, columns: [...state.columns, { ...action.newColumn, id: shortid() }] };
+
+    case 'ADD_CARD':
+      return { ...state, cards: [...state.cards, { ...action.payload, id: shortid(), columnId: 2 }] }
+    default: return state;
+  }
 };
 
 const store = createStore(
