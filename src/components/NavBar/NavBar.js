b4b196646fe7e6560982d6527758c69f2e7d4432
Adrianna Nowak <adriannanowakk@gmail.com>
2023-07-01 13:51:08 +0200

    add icon and nav links

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,31 @@
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './NavBar.module.scss';
+import Container from "../Container/Container";
+
+
+const NavBar = () => {
+
+
+  return (
+    <nav className={styles.navBar}>
+      <Container>
+        <div className={styles.navWrapper}>
+          <div className={styles.navIcon}>
+            <a href='/'>
+              <span className='fa fa-tasks'></span>
+            </a>
+          </div>
+          <div className={styles.navLinks}>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/favorite">Favorite</a></li>
+              <li><a href="/about">About</a></li>
+            </ul>
+          </div>
+        </div>
+      </Container>
+    </nav>
+  );
+};
+
+export default NavBar;
